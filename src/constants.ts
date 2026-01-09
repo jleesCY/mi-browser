d9a4bc6a358ccc146eaf9c1c9d90020918c729c4
Joshua Lees <82112905+jleesCY@users.noreply.github.com>
2026-01-08 20:01:08 -0500

    pre_0.4.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { Dimensions, Platform, UIManager } from 'react-native';
 
-export const APP_VERSION = "0.3.3";
+export const APP_VERSION = "0.4.0";
 
 if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
     UIManager.setLayoutAnimationEnabledExperimental(true);
