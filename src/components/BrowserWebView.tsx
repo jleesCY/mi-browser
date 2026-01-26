04e53c57f1d9960babc10c0a9291c1d92567bcb6
Joshua Lees <82112905+jleesCY@users.noreply.github.com>
2026-01-26 18:14:35 -0500

    fix user agent string

--- src/components/BrowserWebView.tsx
+++ src/components/BrowserWebView.tsx
@@ -408,7 +408,7 @@ export const BrowserWebView = forwardRef<WebView, BrowserWebViewProps>(({
         javaScriptEnabled={jsEnabled}
         userAgent={isDesktop
             ? "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-            : "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"}
+            : "Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"}
         sharedCookiesEnabled={!blockCookies}
         cacheEnabled={true}
         domStorageEnabled={true}
